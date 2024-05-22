2df477fa0b09c722b02391607c97894f382f14f5
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-05-22 15:04:15 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "asdfghjkjhgfdsdfgh"
+    bucket = "vsv-vsv-vsv"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
